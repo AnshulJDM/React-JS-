9b99cd42afc8ac4d97e4c3d2674cc941332ace23
AnshulJDM <159455460+AnshulJDM@users.noreply.github.com>
2024-12-12 17:07:17 +0530

    Form1.jsx

--- firstapp/Form1.jsx
+++ firstapp/Form1.jsx
@@ -1,51 +1,62 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from 'react';
 
-export default function Form() {
-  const [name, setName] = useState("");
-  const [pass, setPass] = useState("");
-  const [arr, setArr] = useState([]);
+const Form = () => {
+    
+    const [name, setName] = useState("")
+    const [pass, setPassword] = useState("")
+    const [arr, setArr] = useState([]);
+
+    const handleform = (e) => {
+        e.preventDefault();
+        console.log(name, pass)
+        setArr([...arr, { name: name, pass: pass }]);
+        setName("");
+        setPassword("");
+        e.target.reset();
+    }
 
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter Your Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <br /> <br />
-        <input
-          type="Password"
-          placeholder="Enter Your Password"
-          value={pass}
-          onChange={(e) => setPass(e.target.value)}
-        />
-        <br />
-        <br />
-        <button type="submit">submit</button>
-      </form>
-      <br />
-      <br />
-      <table border={1}>
-        <thead>
-          <tr>
-            <th>Sr No.</th>
-            <th>Name</th>
-            <th>password</th>
-          </tr>
-        </thead>
-        <tbody>
-          {arr.map((ele, i) => (
-            <tr key={i}>
-              <td>{i + 1}</td>
-              <td>{ele.name}</td>
-              <td>{ele.password}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+
+    return (
+        <div>
+
+            <form onSubmit={handleform}>
+                <input type="text"
+                    placeholder='Enter Name'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)} /><br /><br />
+                <input type="password"
+                    placeholder='Enter Password'
+                    value={pass}
+                    onChange={(e) => setPassword(e.target.value)} /><br /><br />
+                <button type='submit'>Submit</button>
+            </form>
+            <br /><br /><br />
+
+            <table border={1}>
+                <thead>
+                    <tr>
+                        <th>Sr No</th>
+                        <th>name</th>
+                        <th>password</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {arr.map((ele, index) => (
+                        <tr key={index}>
+                            <td>{index + 1}</td>
+                            <td>{ele.name}</td>
+                            <td>{ele.pass}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+
+
+        </div>
+    )
 }
+
+export default Form;
